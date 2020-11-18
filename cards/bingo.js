60fe54d85404749cfe088cc72cc287495f59b451
korey <korey.hinton@gmail.com>
2020-11-18 07:08:19 -0500

    Added progress tracking

--- cards/bingo.js
+++ cards/bingo.js
@@ -9,8 +9,8 @@ function bingoCheck() {
     }
     bingo=true
     clearInterval(intervalId);
-    set_iframe(null);
-    alert("BINGO!!! Round="+roundno)
+    document.getElementsByTagName("div")[0].innerHTML=caption("<button onclick='click_bingo(this)'>Bingo</button>")
+    //alert("BINGO!!! Round="+roundno)
 }
 
 function paint(e) {
@@ -28,6 +28,26 @@ function paint(e) {
     }
 }
 
+function click_bingo(btn) {
+    set_iframe(null);
+    btn.parentNode.removeChild(btn)
+}
+
+function debug_bingo() {
+    tdEls=document.getElementsByTagName("td")
+    var c=0;
+    while (c<numcolors){
+    for (var i=0; i<tdEls.length; i++) {
+        td=tdEls[i]
+        obj={}
+        obj.target=td
+        paint(obj)
+    }
+    c+=1;
+    if (c<numcolors)    next_color()
+    }
+}
+
 function set_iframe(c) {
     urlParms="?thumbnail=t"
     if (c!=null) urlParms+="&color="+c
@@ -37,33 +57,77 @@ function set_iframe(c) {
 
 roundno=0
 color=-1
+numcolors=-1
+
+function play() {
+    playbtn=document.getElementById('playbtn')
+    playbtn.parentElement.removeChild(playbtn)
+
+    // events
+    els=document.getElementsByTagName("td")
+    for (var i=0; i<els.length; i++) {
+        els[i].onmouseover = paint;
+    }
+
+    intervalId=setInterval(next_color, 13000)
+}
+
+function caption(actionBtnHtml) {
+    if (numcolors < 0) {
+        els=document.getElementsByTagName("td")
+        var maxcolor=-1
+        for (var i=0; i<els.length; i++) {
+            var classes=els[i].classList
+            for (var j=0;j<classes.length; j++) {
+                if (classes[0].startsWith("c")) {
+                    test=parseInt(classes[0].replace("c",""))
+                    if (test > maxcolor) {
+                        maxcolor=test
+                    }
+                }
+            }
+        }
+        numcolors=maxcolor+1
+    }
+    return "Round " +roundno + " (Color "+(color+1) + " of "+(numcolors)+") "+actionBtnHtml
+}
+
 function round() {
+        // start of round:
+        roundno += 1
+        if (roundno==1) {
+            divEl=document.createElement("div")
+            divEl.innerHTML=caption("<button onclick='play()' id='playbtn'>Play</button>")
+            divEl.style.padding = "10px"
+            document.body.insertBefore(divEl, document.getElementsByTagName('iframe')[0])
+        } else {
+            divEl=document.getElementsByTagName("div")[0]
+            divEl.innerHTML=caption("")
+        }
+}
+
+function next_color() {
     color += 1
-    roundno += 1
     var els = document.getElementsByClassName('c'+color);
     if (els.length == 0) {
         color=0
-        //els=document.getElementsByClassName('c'+color);
+    }
+    if (color==0) {
+        round()
+    } else {
+        document.getElementsByTagName("div")[0].innerHTML = caption("")
     }
     set_iframe('c'+color);
     //document.body.style.backgroundColor = window.getComputedStyle(els[0]).backgroundColor;
 }
 
 if (new URL(location.href).searchParams.get("thumbnail") == null) {
-    // events
-    els=document.getElementsByTagName("td")
-    for (var i=0; i<els.length; i++) {
-        els[i].onmouseover = paint;
-    }
     // iframe
     iframe = document.createElement('iframe')
     //iframe.src=location.href+"?thumbnail=t"
     document.body.appendChild(iframe)
 
-    // start round
-    alert("Wait for other players to open the "+document.title+" on their own device and then click OK to play");
-    round()
-    intervalId=setInterval(round, 13000);
+    next_color()
 } else {
     linkEl=document.createElement("link")
     linkEl.type="text/css"
